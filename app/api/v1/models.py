37f0d14487b07c0cefe5cd431fd8b9d3f7efdf11
ipaullly <ipaully22@gmail.com>
2018-11-07 00:53:12 +0300

    [finishes #161766269] Add class and methods docstrings

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -2,11 +2,16 @@
 parcels = []
 
 class Parcels:
-
+    """
+    Class with CRUD functionalities on the Parcels resource
+    """
     def __init__(self):
         self.db = parcels
 
     def create_order(self, item, pickup, dest, pricing):
+        """
+        instance method to generate new entry into delivery orders list
+        """
         payload = {
             "id" : len(self.db) + 1,
             "itemName" : item,
@@ -17,9 +22,15 @@ class Parcels:
         self.db.append(payload)
 
     def order_list(self):
+        """
+        retrieves entire list of delivery orders
+        """
         return self.db
     
     def retrieve_single_order(self, parcelID):
+        """
+        retrive a single order by id
+        """
         order_by_id = [parc for parc in self.db if parc['id'] == parcelID][0]
         return order_by_id
 
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -5,8 +5,13 @@ from .models import Parcels
 order = Parcels()
 
 class ParcelList(Resource):
-
+    """
+    class for Create order and retrieve list of orders API endpoints
+    """
     def post(self):
+        """
+        post method to add new order to list of orders
+        """
         data = request.get_json()
         item = data['item']
         pickup = data['pickup']
@@ -21,16 +26,23 @@ class ParcelList(Resource):
         }), 201)
     
     def get(self):
+        """
+        get method to retrieve list of all orders
+        """
         resp = order.order_list()
         return make_response(jsonify({
             "message" : "ok",
             "Delivery Orders" : resp
         }), 200)
 
-
 class IndividualParcel(Resource):
-
+    """
+    class for API endpoints for retrieving single order and cancelling particular order
+    """
     def get(self, id):
+        """
+        get method to retrieve order by id
+        """
         single = order.retrieve_single_order(id)
         return make_response(jsonify({
             "message" : "Ok",
