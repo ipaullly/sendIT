101cd2c193b179e9c551ad1d531af7c9d6a1ca2b
ipaullly <ipaully22@gmail.com>
2018-11-07 09:09:01 +0300

    [finishes #161773596] add doctsrtings to test class

--- app/tests/v1/test_parcels.py
+++ app/tests/v1/test_parcels.py
@@ -3,13 +3,7 @@ import unittest
 import json
 
 class TestPracelCreation(unittest.TestCase):
-    """
-    class for Parcels test case
-    """
     def setUp(self):
-        """
-        Initialize app and define test variables
-        """
         create_app().testing = True
         self.app = create_app().test_client()
         self.data = {
@@ -20,18 +14,13 @@ class TestPracelCreation(unittest.TestCase):
         }
 
     def test_POST_create_delivery_order(self):
-        """
-        Test whether API can create a new delivery order via POSt request
-        """
         response = self.app.post('/api/v1/parcels', data=json.dumps(self.data), content_type='application/json')
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 201)
         self.assertIn('seven ballons', str(result))
     
     def test_GET_delivery_orders_list(self):
-        """
-        Test if API can retrieve a list of delivery orders
-        """
+        """Test if API can retrieve a list of delivery orders"""
         response = self.app.post('/api/v1/parcels', data=json.dumps(self.data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
         response = self.app.get('/api/v1/parcels', content_type='application/json')
