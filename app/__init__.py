e22c5239b753233f1b36de874693ef0f3b728a94
ipaullly <ipaully22@gmail.com>
2018-11-08 00:33:47 +0300

    [starts #161796710] Create authentication blueprint in project directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, Blueprint
 from .api.v1 import version1
+from .auth.v1 import auth_version1
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(version1)
-
+    app.register_blueprint(auth_version1)
+    
     return app
