0d6055eaece7154081da418ba289701ecdfccfd6
ipaullly <ipaully22@gmail.com>
2018-11-08 08:38:46 +0300

    write test for registration endpoint and additional logic in its view

--- app/auth/v1/views.py
+++ app/auth/v1/views.py
@@ -8,9 +8,16 @@ class Registration(Resource):
         email = data['email']
         password = data['password']
 
-        new_user = User(email=email, password=password)
-        new_user.add_user()
+        if not User.get_user_by_email(email):
+            new_user = User(email=email, password=password)
+            new_user.add_user()
         
-        return make_response(jsonify({
-            'message' : 'you have successfully registered an account'
-        }), 201)
+            return make_response(jsonify({
+                'message' : 'you have successfully registered an account'
+            }), 201)
+        else:
+            response = {
+                'message': 'Account with provided email exists. please login'
+            }
+
+            return make_response((jsonify(response)), 202)
--- app/tests/v1/test_registration.py
+++ app/tests/v1/test_registration.py
@@ -13,13 +13,22 @@ class AuthTestCase(unittest.TestCase):
             'email' : 'test@hotmail.com',
             'password' : 'holy_water'
         }
-    
+        
+    """
     def test_signup(self):
-        response = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
-        res = json.loads(response.data)
-        self.assertEqual(response.status_code, 201)
+        response2 = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        res = json.loads(response2.data)
+        self.assertEqual(response2.status_code, 201)
         self.assertEqual(res['message'], "you have successfully registered an account")
+    """
 
+    def test_if_registered(self):
+        response1 = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        self.assertEqual(response1.status_code, 201)
+        duplicate_signup = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        self.assertEqual(duplicate_signup.status_code, 202)
+        res = json.loads(duplicate_signup.data)
+        self.assertEqual(res['message'], 'Account with provided email exists. please login')
 
 
 
