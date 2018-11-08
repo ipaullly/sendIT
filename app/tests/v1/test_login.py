9e3308458b37ad49ae3e22abc95f2af333fbeea6
ipaullly <ipaully22@gmail.com>
2018-11-08 23:13:54 +0300

    [finishes #161820120] write minimal tests for sign in endpoint

---
+++ app/tests/v1/test_login.py
@@ -0,0 +1,40 @@
+from ... import create_app
+import unittest
+import json
+
+class LoginTestCase(unittest.TestCase):
+    """
+    test class for the registration endpoint
+    """
+    def setUp(self):
+        create_app().testing = True
+        self.app = create_app().test_client()
+        self.mock_data = {
+            'email' : 'test@chocoly.com',
+            'password' : 'balerion'
+        }
+        self.not_user_data = {
+            'email' : 'not_user@chocoly.com',
+            'password' : 'silmarillion'
+        }
+
+    def test_user_signin(self):
+        #test if a registered user can log in
+        res = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        signin_res = self.app.post('/auth/v1/login', data=json.dumps(self.mock_data), content_type='application/json')
+        result = json.loads(signin_res.data)
+        self.assertEqual(result['message'], "Successfully logged in")
+        self.assertEqual(signin_res.status_code, 200)
+        self.assertTrue(result['authentication token'])
+
+    def test_non_registered_user(self):
+        #test that unregistered user cannot log in
+        res = self.app.post('/auth/v1/login', data=json.dumps(self.mock_data), content_type='application/json')
+        result = json.loads(res.data)
+        self.assertEqual(res.status_code, 500)
+
+
+if __name__ == "__main__":
+    unittest.main()
+        
--- app/tests/v1/test_register.py
+++ app/tests/v1/test_register.py
@@ -13,24 +13,25 @@ class AuthTestCase(unittest.TestCase):
             'email' : 'test@hotmail.com',
             'password' : 'holy_water'
         }
-        
+        self.mock_data2 = {
+            'email' : 'qarth@hotmail.com',
+            'password' : 'jade_sea'
+        }
+          
     """
     def test_signup(self):
         response2 = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
         res = json.loads(response2.data)
-        self.assertEqual(response2.status_code, 201)
         self.assertEqual(res['message'], "you have successfully registered an account")
     """
 
     def test_if_registered(self):
-        response1 = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        response1 = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data2), content_type='application/json')
         self.assertEqual(response1.status_code, 201)
-        duplicate_signup = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data), content_type='application/json')
+        duplicate_signup = self.app.post('/auth/v1/register', data=json.dumps(self.mock_data2), content_type='application/json')
         self.assertEqual(duplicate_signup.status_code, 202)
         res = json.loads(duplicate_signup.data)
         self.assertEqual(res['message'], 'Account with provided email exists. please login')
-
-
 
 if __name__ == "__main__":
     unittest.main()
