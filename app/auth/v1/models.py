31b405a64994b8c4bd2794075b05870e636b2dd0
ipaullly <ipaully22@gmail.com>
2018-11-13 17:11:43 +0300

    [starts #161920708] create utilities and move decode token function into it

--- app/auth/v1/models.py
+++ app/auth/v1/models.py
@@ -82,22 +82,6 @@ class User(Parent):
             return token
         except Exception as err:
             return str(err)
-        
-    @staticmethod
-    def decode_token(token):
-        """
-        method to decode the token generated during login
-        """
-        try:
-            #attempt to decode token using SECRET_KEY variable
-            payload = jwt.decode(token, current_app.config.get('SECRET_KEY'))
-            return payload['id']
-        except  jwt.ExpiredSignatureError:
-            # expired token returns an error string
-            return "Token expired. please login again to generate fresh token"
-        except jwt.InvalidTokenError:
-            #the token is not valid, throw error
-            return "Unworthy token. Please login to get fresh authorization"
 
     @classmethod
     def get_user_by_email(cls, email):
