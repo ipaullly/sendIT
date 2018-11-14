360b4fa82603da44ee1542070c2396bfb0103ccb
ipaullly <ipaully22@gmail.com>
2018-11-14 18:12:38 +0300

    [starts #161957214] Add validation for correct email pattern

--- app/auth/v1/views.py
+++ app/auth/v1/views.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask import make_response,jsonify, request
 from .models import User
-from ...utilities.validation_functions import check_for_space
+from ...utilities.validation_functions import check_for_space, check_email_format
 
 class Registration(Resource):
     """
@@ -13,19 +13,25 @@ class Registration(Resource):
         password = data['password']
         
         if check_for_space(email):
-            if not User.get_user_by_email(email):
-                new_user = User(email=email, password=password)
-                new_user.add_user()
+            if check_email_format(email):
+                if not User.get_user_by_email(email):
+                    new_user = User(email=email, password=password)
+                    new_user.add_user()
         
-                return make_response(jsonify({
-                    'message' : 'you have successfully registered an account'
-                }), 201)
+                    return make_response(jsonify({
+                        'message' : 'you have successfully registered an account'
+                    }), 201)
+                else:
+                    response = {
+                        'message': 'Account with provided email exists. please login'
+                    }
+
+                    return make_response(jsonify(response), 409)
             else:
                 response = {
-                    'message': 'Account with provided email exists. please login'
+                    'message' : 'Invalid email format'
                 }
-
-                return make_response(jsonify(response), 409)
+                return make_response(jsonify(response), 400)
         else:
             response = {
                 'message' : 'Whitespaces are invalid inputs'
