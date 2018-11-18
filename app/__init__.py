e4de9e8d33aafd2cd997077f0663ec3522f2ef8f
ipaullly <ipaully22@gmail.com>
2018-11-18 16:02:01 +0300

    [starts #162036641] create configuration to create database tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask, Blueprint
 from .api.v1 import version1
+#from .api.v2 import vern2
 from .auth.v1 import auth
+from .db_config import create_tables
 #from db_config import create_tables, destroy_tables
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    #create_tables()
+    create_tables()
     app.config.from_pyfile('config.py')
     app.register_blueprint(version1)
+    #app.register_blueprint(vern2)
     app.register_blueprint(auth)
-
     return app
