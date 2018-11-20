080cec5d632dca4219647f00d86f3228219ca0d7
ipaullly <ipaully22@gmail.com>
2018-11-20 11:17:07 +0300

    [starts #162080286] rewrite validation logic for checking spaces

--- app/utilities/validation_functions.py
+++ app/utilities/validation_functions.py
@@ -2,10 +2,7 @@ import re
 
 def check_for_space(varib):
     output = varib.strip(" ")
-    if output:
-        return True
-    else:
-        return False
+    return output
 
 def check_email_format(varib):
     match = re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]*\.*[com|org|edu]{3}$)",varib)
