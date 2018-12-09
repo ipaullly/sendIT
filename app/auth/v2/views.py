c6f4e862da25b377cfb2a9142a0150632f02808d
ipaullly <ipaully22@gmail.com>
2018-12-09 22:22:17 +0300

    add str() function

--- app/auth/v2/views.py
+++ app/auth/v2/views.py
@@ -106,7 +106,7 @@ class SignIn(Resource):
             return make_response(jsonify(response), 401)
         response = {
             'message' : 'Successfully logged in',
-            'data' : auth_token.decode('ASCII')
+            'data' : str(auth_token)
         }
         return make_response(jsonify(response), 200)
       
