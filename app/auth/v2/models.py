41a5637603d7869dc49a1a3984af74dd3e1e385c
ipaullly <ipaully22@gmail.com>
2018-12-10 11:53:38 +0300

    add decode() method

--- app/auth/v2/models.py
+++ app/auth/v2/models.py
@@ -80,7 +80,7 @@ class User:
                 'iat' : datetime.utcnow(),
                 'id' : userID
             }
-            token = jwt.encode(payload, os.environ.get('SECRET_KEY'), 'HS256')
+            token = jwt.encode(payload, os.environ.get('SECRET_KEY'))
             return token
         except Exception as err:
             return str(err)
--- app/auth/v2/views.py
+++ app/auth/v2/views.py
@@ -107,11 +107,11 @@ class SignIn(Resource):
             }
             return make_response(jsonify(response), 401)
         
-        str_token = str(object=auth_token, encoding='utf-8', errors='strict')
+        #str_token = str(object=auth_token, encoding='utf-8', errors='strict')
 
         response = {
             'message' : 'Successfully logged in',
-            'data' : str_token
+            'data' : auth_token.decode("utf-8")
         }
         return make_response(jsonify(response), 200)
       
