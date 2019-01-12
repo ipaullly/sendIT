fa0e178b4e0e097ba4ab90aec6374ff7d04d7752
ipaullly <ipaully22@gmail.com>
2019-01-12 15:29:23 +0300

    Finish logic for diplaying error messages

--- js/login.js
+++ js/login.js
@@ -15,13 +15,19 @@ function logIn(){
         })
     })
     .then((res) => {
-        if (res.ok){
+      /*  if (res.ok){
             return res.json().then((myJson) => {
                 output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-size: 1.3em;font-family: 'Boogaloo', cursive;">${myJson.message}</p>`;
                 return document.getElementById('signupresponse').innerHTML = output;
             })
+        }*/
+        if (res.status == 400){
+            return res.json().then((myJson) => {
+                output = `<p style="background: #a60000;color: white;text-align: center;padding: 20px;font-size: 1.3em;font-family: 'Boogaloo', cursive;">${myJson.message}</p>`;
+                return document.getElementById('loginresponse').innerHTML = output;
+            })
         }
-        if (res.status == 400 || res.status == 401){
+        if (res.status == 401){
             return res.json().then((myJson) => {
                 output = `<p style="background: #a60000;color: white;text-align: center;padding: 20px;font-size: 1.3em;font-family: 'Boogaloo', cursive;">${myJson.message}</p>`;
                 return document.getElementById('loginresponse').innerHTML = output;
