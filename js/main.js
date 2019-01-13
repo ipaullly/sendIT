40ccef9da4b04238ac35b3c4ed3198bc36302933
ipaullly <ipaully22@gmail.com>
2019-01-13 21:02:10 +0300

    [starts #163176321] decode jwt token to retrieve user id

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const registerButton = document.getElementById('signupbutton');
 const loginButton = document.getElementById('loginbut');
 const createOrderButton = document.getElementById('createparcel');
+const userOrderButton = document.getElementById('userorders');
 
 function signUp(){
     let output;
@@ -69,6 +70,12 @@ function logIn(){
     });
 }
 
+function decodeJwt (token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(window.atob(base64));
+}
+
 function loginResponse(){
     let response = sessionStorage.getItem( 'loginResponse' );
     let output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-family: 'Boogaloo', cursive;">${response}</p>`;
@@ -113,6 +120,38 @@ function createParcel(){
 
 }
 
+function retrieveUserOrders(){
+    let token = sessionStorage.getItem( 'token' ).replace("'", "");
+    token = token.substr(0, token.length-1); 
+    let decodedToken = decodeJwt(token);
+    decodedTokenId = Object.values(decodedToken)[2];
+    fetch('https://sendit-versiontwo.herokuapp.com/api/v2/users/'+decodedTokenId+'/parcels', {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": null,
+            "Content-Type": "application/json; charset=UTF-8",
+            "Authorization": "Bearer " + token
+        }
+    })
+    .then((res) => {
+        if (res.ok){
+            return res.json().then((myJson) => {
+                output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-size: 1.3em;font-family: 'Boogaloo', cursive;">${myJson.message}</p>`;
+                return document.getElementById('redirectedlogin').innerHTML = output;
+            })
+        }
+        if (res.status == 400){
+            return res.json().then((myJson) => {
+                output = `<p style="background: #a60000;color: white;text-align: center;padding: 20px;font-size: 1.3em;font-family: 'Boogaloo', cursive;">${myJson.message}</p>`;
+                return document.getElementById('redirectedlogin').innerHTML = output;
+            })
+        }
+    });
+
+}
+
 if (registerButton){
     registerButton.addEventListener('click', signUp);
 }
@@ -121,4 +160,7 @@ if (loginButton){
 }
 if (createOrderButton){
     createOrderButton.addEventListener('click', createParcel);
+}
+if (userOrderButton){
+    userOrderButton.addEventListener('click', retrieveUserOrders);
 }
