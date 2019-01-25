11dec548befc2a9fae965fd356ca7fa9c4445103
ipaullly <ipaully22@gmail.com>
2019-01-25 14:30:36 +0300

    add jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    verbose: true,
+    testURL: "http://localhost:8000/",
+    "setupFiles": [
+      "./jest/globals.js"
+    ], 
+   "coverageDirectory": "./tests/coverage",
+
+   "coveragePathIgnorePatterns": [
+      "./jest",
+    ],
+  };
